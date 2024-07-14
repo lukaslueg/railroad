4d0fa370b40860b3de1d4a896f8298af23d39277
Lukas Lueg <lukas.lueg@gmail.com>
2024-07-14 20:39:37 +0200

    Add `encode_attribute`

--- tests/test_lib.rs
+++ tests/test_lib.rs
@@ -2,3 +2,11 @@
 fn test_encode_minimal() {
     assert_eq!(railroad::svg::encode_minimal("foo<bar>"), "foo&lt;bar&gt;");
 }
+
+#[test]
+fn test_encode_attribute() {
+    assert_eq!(
+        railroad::svg::encode_attribute("fö bör"),
+        "f&#xF6;&#x20;bo\u{308}r"
+    );
+}
